33614d711711bb575a34d8d920f08febe12d94f3
Leandro Moreira <ilegra.oliveira@agi.com.br>
2024-01-14 14:45:57 -0300

    seção 06

---
+++ secao06/src/test/java/br/com/leomoreiradev/spring3/StatupTests.java
@@ -0,0 +1,13 @@
+package br.com.leomoreiradev.spring3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
