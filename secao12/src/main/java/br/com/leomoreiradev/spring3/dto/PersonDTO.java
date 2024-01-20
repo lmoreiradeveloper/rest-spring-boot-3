fb3353bc510a25e41b60e70e51a792f6a4be05f3
Leandro Moreira <ilegra.oliveira@agi.com.br>
2024-01-20 19:42:58 -0300

    seção 12

---
+++ secao12/src/main/java/br/com/leomoreiradev/spring3/dto/PersonDTO.java
@@ -0,0 +1,82 @@
+package br.com.leomoreiradev.spring3.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@JsonPropertyOrder({"id","gender","lastName","address","firstName"}) //para ordenar campos na resposta
+public class PersonDTO implements Serializable {
+   private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    @JsonProperty("first_name") //para mostrar outro nome na resposta do json
+    private String firstName;
+
+    private String lastName;
+
+    @JsonIgnore // omite esse campo na resposta do json
+    private String address;
+
+
+    private String gender;
+
+    public PersonDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonDTO person = (PersonDTO) o;
+        return Objects.equals(id, person.id) && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(address, person.address) && Objects.equals(gender, person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, address, gender);
+    }
+}
