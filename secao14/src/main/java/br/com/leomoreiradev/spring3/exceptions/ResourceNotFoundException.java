712d9826cd8d24bb8dce4f200ab5c883d0c0353f
Leandro Moreira <ilegra.oliveira@agi.com.br>
2024-01-22 12:24:15 -0300

    hateoas-secao-14-nao-implementada

--- secao14/src/main/java/br/com/leomoreiradev/spring3/exceptions/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package br.com.leomoreiradev.spring3.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    private static final long serialVersionUID = 1L;
-
-    public ResourceNotFoundException(String mensagem) {
-        super(mensagem);
-    }
-}
