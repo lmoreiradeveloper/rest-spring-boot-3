87313ce207fedb71a6dacfc3b0e92c50bee18469
Leandro Moreira <ilegra.oliveira@agi.com.br>
2024-01-22 12:24:52 -0300

    hateoas-secao-14-nao-implementada

---
+++ hateoas-secao-14-nao-implementada/src/main/java/br/com/leomoreiradev/spring3/util/Mediatype.java
@@ -0,0 +1,10 @@
+package br.com.leomoreiradev.spring3.util;
+
+public class Mediatype {
+
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String APPLICATION_XML = "application/xml";
+    public static final String APPLICATION_YML = "application/x-yaml";
+
+
+}
