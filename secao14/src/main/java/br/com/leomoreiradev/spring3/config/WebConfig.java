089751aee1efc720de846cb5b394159cd0b9a31c
Leandro Moreira <ilegra.oliveira@agi.com.br>
2024-01-22 11:22:10 -0300

    seção 13

---
+++ secao14/src/main/java/br/com/leomoreiradev/spring3/config/WebConfig.java
@@ -0,0 +1,51 @@
+package br.com.leomoreiradev.spring3.config;
+
+import br.com.leomoreiradev.spring3.serialization.converter.YamlJackson2HttpMesageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    //Apenas para serializar em yaml
+    private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("applicaion/x-yaml");
+
+    /*configuração para negociação de conteudo para suportar o formato xml
+    VIA QUERY PARAM localhost:8080/person/1?mediaType=xml
+    * */
+//    @Override
+//    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//     configurer.favorParameter(true)
+//             .parameterName("mediaType")
+//             .ignoreAcceptHeader(true)
+//             .useRegisteredExtensionsOnly(false)
+//             .defaultContentType(MediaType.APPLICATION_JSON)
+//             .mediaType("json", MediaType.APPLICATION_JSON)
+//             .mediaType("xml", MediaType.APPLICATION_XML);
+//    }
+
+
+    /*configuração para negociação de conteudo para suportar o formato xml
+    VIA HEADER PARAM localhost:8080/person/1
+    * */
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("xml", MEDIA_TYPE_APPLICATION_YML);
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+       converters.add(new YamlJackson2HttpMesageConverter());
+    }
+}
